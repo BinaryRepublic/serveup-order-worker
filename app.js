82a935a1a7d696af4f59707049e06a0d834bd0dd
Henrik Engelbrink <hengel2810@gmail.com>
2018-01-19 15:44:39 +0100

    Added ro-realm submodule Implemented socket

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 'use strict';
-const RealmController = require('./src/realm/RealmController.js');
+const RealmController = require('./src/RealmController.js');
+const socketIO = require('socket.io');
 
-let realmController = new RealmController("./DataRealm", function(newOrder) {
-	console.log("New order: " + JSON.stringify(newOrder));
+var socket = socketIO();
+socket.on('connection', function(client){
+	console.log("client on 9000");
 });
+socket.listen(9000);
+
+let realmController = new RealmController(function() {
+	realmController.setupListener(function(newOrder){
+		socket.emit('neworder', JSON.stringify(newOrder));
+	});
+});
+
