449eb1a8ab63da30c46ac6c0c0345b2fbb8618f1
Henrik Engelbrink <hengel2810@gmail.com>
2018-01-21 14:22:54 +0100

    Updated travis file

--- socket_test.js
+++ socket_test.js
@@ -2,7 +2,7 @@ const io = require('socket.io-client');
 
 var restID = process.argv[2];
 
-const socket = io('http://localhost:9000');
+const socket = io('http://138.68.71.39:9000');
 socket.on('connect', function(){
 	socket.emit("restaurantId", restID);
 });
