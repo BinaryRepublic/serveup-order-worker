f9b2e0f397910f7ee9ac82118e2a5ea98d3b66d9
tobiastrinks <tobias.trinks@code.berlin>
2018-03-23 09:40:34 +0100

    added eslint

--- src/SocketController.js
+++ src/SocketController.js
@@ -1,40 +1,39 @@
-"use_strict"
+'use strict';
 const socketIO = require('socket.io');
 
 class SocketController {
-	constructor() {
-		this.clients = [];
-		this.socket = socketIO();
-		this.socket.on('connect', this.onConnect.bind(this));
-		this.socket.listen(9000);
-	}
-	onConnect(client) {
-		client.on("restaurantId", this.onRestaurantId.bind(this, client))
-		client.on('disconnect', this.onDisconnect.bind(this, client));
-	}
-	onRestaurantId(client, id) {
-		client.restaurantId = id;
-		this.clients.push(client);
-		console.log("+ " + this.clients.length + " clients connected");
-	}
-	onDisconnect(client, reason) {
-		this.clients.pop(client);
-		console.log("- " + this.clients.length + " clients connected")
-	}
-	emitNewOrder(order) {
-		var restaurantId = order.restaurantId;
-		var client = this.clients.find(this.findClient.bind(null, restaurantId));
-		if(client) {
-			client.emit('neworder', JSON.stringify(order));
-		}
-		else {
-			console.error("emitNewOrder no client found for " + restaurantId);
-		}
-	}
-	findClient(restaurantId, client) { 
-		if(client.restaurantId === restaurantId) {
-			return client;
-		}
-	}
+    constructor () {
+        this.clients = [];
+        this.socket = socketIO();
+        this.socket.on('connect', this.onConnect.bind(this));
+        this.socket.listen(9000);
+    }
+    onConnect (client) {
+        client.on('restaurantId', this.onRestaurantId.bind(this, client));
+        client.on('disconnect', this.onDisconnect.bind(this, client));
+    }
+    onRestaurantId (client, id) {
+        client.restaurantId = id;
+        this.clients.push(client);
+        console.log('+ ' + this.clients.length + ' clients connected');
+    }
+    onDisconnect (client, reason) {
+        this.clients.pop(client);
+        console.log('- ' + this.clients.length + ' clients connected');
+    }
+    emitNewOrder (order) {
+        var restaurantId = order.restaurantId;
+        var client = this.clients.find(this.findClient.bind(null, restaurantId));
+        if (client) {
+            client.emit('neworder', JSON.stringify(order));
+        } else {
+            console.error('emitNewOrder no client found for ' + restaurantId);
+        }
+    }
+    findClient (restaurantId, client) {
+        if (client.restaurantId === restaurantId) {
+            return client;
+        }
+    }
 }
 module.exports = SocketController;
