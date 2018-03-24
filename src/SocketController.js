e93ed294755675762f0be7123837fc9aab64b717
tobiastrinks <tobias.trinks@code.berlin>
2018-03-24 15:41:26 +0100

    Merge branch 'develop' of https://github.com/restaurantorder/ro-order-worker into develop
    # Conflicts:
    #	package-lock.json
    #	package.json
    #	src/SocketController.js
    
    - added authorization

--- src/SocketController.js
+++ src/SocketController.js
@@ -1,22 +1,28 @@
 'use strict';
 const socketIO = require('socket.io');
 const AuthApiInterface = require('./AuthApiInterface');
+const Authorization = require('./Authorization');
 
 class SocketController {
     constructor () {
         this.clients = [];
+        this.accountId = null;
+        this.authorization = new Authorization();
+        // authentication middleware
         let authApi = new AuthApiInterface();
         this.socket = socketIO();
         this.socket.use((socket, next) => {
             let token = socket.handshake.query.token;
             if (token) {
                 authApi.access(token).then(resp => {
+                    this.accountId = resp.clientId;
                     next();
-                }).catch((err) => {
+                }).catch(() => {
                     let error = {
                         type: 'ACCESS_TOKEN_INVALID',
                         msg: 'Please send a valid access-token in the socket query'
                     };
+                    console.log(error)
                     next(new Error(JSON.stringify(error)));
                 });
             } else {
@@ -24,39 +30,51 @@ class SocketController {
                     type: 'NO_ACCESS_TOKEN',
                     msg: 'Please send a valid access-token in the socket query'
                 };
+                console.log(error)
                 next(new Error(JSON.stringify(error)));
             }
         });
         this.socket.on('connect', this.onConnect.bind(this));
         this.socket.listen(9000);
     }
-	onConnect(client) {
-		client.on('restaurantId', this.onRestaurantId.bind(this, client))
-		client.on('disconnect', this.onDisconnect.bind(this, client));
-	}
-	onRestaurantId(client, id) {
-		client.restaurantId = id;
-		this.clients.push(client);
-		console.log("+ " + this.clients.length + " clients connected");
-	}
-	onDisconnect(client, reason) {
-		this.clients.pop(client);
-		console.log("- " + this.clients.length + " clients connected")
-	}
-	emitNewOrder(order) {
-		var restaurantId = order.restaurantId;
-		var client = this.clients.find(this.findClient.bind(null, restaurantId));
-		if(client) {
-			client.emit('neworder', JSON.stringify(order));
-		}
-		else {
-			console.error("emitNewOrder no client found for " + restaurantId);
-		}
-	}
-	findClient(restaurantId, client) {
-		if(client.restaurantId === restaurantId) {
-			return client;
-		}
-	}
+    onConnect (client) {
+        // authorization
+        client.on('restaurantId', this.onRestaurantId.bind(this, client));
+        client.on('disconnect', this.onDisconnect.bind(this, client));
+    }
+    onRestaurantId (client, id) {
+        let authorization = this.authorization.request(this.accountId, 'Restaurant', id);
+        if (authorization && !authorization.error) {
+            client.restaurantId = id;
+            this.clients.push(client);
+            console.log('+ ' + this.clients.length + ' clients connected');
+        } else {
+            console.log('client not authorized');
+            client.emit('err', JSON.stringify({
+                error: {
+                    type: 'NOT_AUTHORIZED',
+                    msg: 'Credentials do not fit to restaurantId'
+                }
+            }));
+        }
+    }
+    onDisconnect (client, reason) {
+        this.clients.pop(client);
+        console.log('- ' + this.clients.length + ' clients connected');
+    }
+    emitNewOrder (order) {
+        var restaurantId = order.restaurantId;
+        var client = this.clients.find(this.findClient.bind(null, restaurantId));
+        if (client) {
+            client.emit('neworder', JSON.stringify(order));
+        } else {
+            console.error('emitNewOrder no client found for ' + restaurantId);
+        }
+    }
+    findClient (restaurantId, client) {
+        if (client.restaurantId === restaurantId) {
+            return client;
+        }
+    }
 }
 module.exports = SocketController;
